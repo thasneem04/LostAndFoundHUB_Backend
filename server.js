338818388c3db1a2c83f28cc39cd21e092f3d504
thasneem04 <thasneem.explorer5@gmail.com>
2026-01-11 20:57:18 +0530

    m

--- server.js
+++ server.js
@@ -8,21 +8,36 @@ const item = require("./routes/itemRoutes");
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: process.env.URL,
-}));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://lost-and-found-hub-frontend.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/items", item);   // ✅ Add item routes
+app.use("/api/items", item); // ✅ Add item routes
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on ${PORT}`));
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
