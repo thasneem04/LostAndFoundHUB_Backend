63047f5d665aff37720dd9388259778bf65beb86
thasneem04 <thasneem.explorer5@gmail.com>
2026-01-11 19:30:46 +0530

    changesserver

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const app = express();
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", cors()); 
 app.use(express.json());
 
 // Routes
